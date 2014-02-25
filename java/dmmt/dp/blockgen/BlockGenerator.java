c49a3cdcc2fb06eb4d9e184a6a33ef06d68bbdd7
ianno1993 <i.lol@hotmail.com>
2014-02-25 20:57:07 +0100

    1.7.2 - 0.0.00A: World Gen prob fixed
    World Gen prob fixed

--- java/dmmt/dp/blockgen/BlockGenerator.java
+++ java/dmmt/dp/blockgen/BlockGenerator.java
@@ -1,4 +1,4 @@
-package dmmt.mod1.blockgen;
+package dmmt.dp.blockgen;
 
 import java.util.Random;
 
@@ -8,7 +8,7 @@ import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import cpw.mods.fml.common.IWorldGenerator;
-import dmmt.mod1.basic.Basic;
+import dmmt.dp.main.Main;
 
 public class BlockGenerator implements IWorldGenerator {
 
@@ -37,22 +37,22 @@ public class BlockGenerator implements IWorldGenerator {
 		//add stones
 		int veinsize = 256;
 		
-		this.addOreSpawn(Basic.andesite, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 5, 255);
+		this.addOreSpawn(Main.andesite, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 5, 255);
 		if(biomeGenBase.biomeName == "taigaHills" || biomeGenBase.biomeName == "jungleHills" || biomeGenBase.biomeName == "desertHills" || biomeGenBase.biomeName == "extremeHills" || biomeGenBase.biomeName == "forestHills" || biomeGenBase.biomeName == "iceMountains" || biomeGenBase.biomeName == "mushroomIsland" || biomeGenBase.biomeName == "mushroomIslandShore"){
-			this.addOreSpawn(Basic.dioriteQuartz, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 5, 255);
+			this.addOreSpawn(Main.dioriteQuartz, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 5, 255);
 		}		
-		this.addOreSpawn(Basic.dolomite, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 5, 255);
-		this.addOreSpawn(Basic.greenGranite, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 20, 64);
-		this.addOreSpawn(Basic.marble, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 5, 45);
-		this.addOreSpawn(Basic.pumice, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 40, 255);
-		this.addOreSpawn(Basic.siltstone, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 25, 60);
+		this.addOreSpawn(Main.dolomite, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 5, 255);
+		this.addOreSpawn(Main.greenGranite, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 20, 64);
+		this.addOreSpawn(Main.marble, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 5, 45);
+		this.addOreSpawn(Main.pumice, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 40, 255);
+		this.addOreSpawn(Main.siltstone, world, random, x, z, 16, 16, veinsize, chanceToSpawn(100, 40, random), 25, 60);
 		
 		//add ores
-		this.addOreSpawn(Basic.titaniumOre, world, random, x, z, 16, 16, 8, 1, 3, 20);
+		this.addOreSpawn(Main.titaniumOre, world, random, x, z, 16, 16, 8, 1, 3, 20);
 		
 		//add watermelonstone in specific biomes
 		if(biomeGenBase.biomeName == "ocean" || biomeGenBase.biomeName == "jungle" || biomeGenBase.biomeName == "jungleHills"){
-			this.addOreSpawn(Basic.watermelonStone, world, random, x, z, 16, 16, 4, 3, 5, 255);
+			this.addOreSpawn(Main.watermelonStone, world, random, x, z, 16, 16, 4, 3, 5, 255);
 		}
 		
 		
@@ -92,7 +92,7 @@ public class BlockGenerator implements IWorldGenerator {
                  int posX = blockXPos + random.nextInt(maxX);
                  int posY = minY + random.nextInt(diffBtwnMinMaxY);
                  int posZ = blockZPos + random.nextInt(maxZ);
-                 (new WorldGenMinable(block.blockID, maxVeinSize)).generate(world, random, posX, posY, posZ);
+                 (new WorldGenMinable(block, maxVeinSize)).generate(world, random, posX, posY, posZ);
           }
     }
     
