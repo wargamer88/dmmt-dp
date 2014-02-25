b62b216e836cc6958b9956db723a147b2278ffe5
wargamer88 <m.van.denzel@home.nl>
2014-02-25 20:52:30 +0100

    1.7.2 - 0.0.00A: fixed melonore

--- java/dmmt/dp/blocks/rawores/WatermelonStone.java
+++ java/dmmt/dp/blocks/rawores/WatermelonStone.java
@@ -2,12 +2,12 @@ package dmmt.dp.blocks.rawores;
 
 import java.util.Random;
 
-import dmmt.dp.main.Main;
-import dmmt.dp.main.ModInfo;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import dmmt.dp.main.ModInfo;
 
 public class WatermelonStone extends Block {
 
@@ -24,10 +24,11 @@ public class WatermelonStone extends Block {
 	/**
      * Returns the ID of the items to drop on destruction.
      */
-    public int idDropped(int par1, Random par2Random, int par3)
-    {
-        return Item.melon.itemID;
-    }
+	public Item getItemDropped(int p_149650_1_,
+            java.util.Random p_149650_2_,
+            int p_149650_3_){
+		return Items.melon;
+	}
 
     /**
      * Returns the quantity of items to drop on block destruction.
